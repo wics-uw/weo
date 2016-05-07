048c1825f80c4405c9359e5222c4c8de98084bd5
Elana Hashman <elana@hashman.ca>
2016-05-07 18:40:13 -0400

    Add setup info for build/installation

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+setuptools.setup(
+    name='weo',
+    version='0.0.1',
+    description='WiCS Electronic Office',
+    url='https://github.com/wics-uw/weo',
+    author='Elana Hashman',
+    author_email='ehashman@wics.uwaterloo.ca',
+    maintainer='Women in Computer Science Systems Committee',
+    maintainer_email='wics-sys@lists.uwaterloo.ca',
+    license='GPLv2',
+    classifiers=[
+        'Development Status :: 4 - Beta',	
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='weo',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'weo=weo.cli:main',
+        ]
+    },
+    install_requires=[
+        'python-ldap',
+        'python-kadmin',
+        'python-dateutil',
+    ],
+)
