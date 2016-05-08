c578966118a365f9216c63eef5275c435d8104b1
Elana Hashman <elana@hashman.ca>
2016-05-08 16:14:50 -0400

    I can't spell

--- weo/cli.py
+++ weo/cli.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_imports
+from __future__ import absolute_import
 
 
 import getopt
